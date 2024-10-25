09929ca41609ccabcf3332ff3f5b8c38863b951a
Alysson Alcântara <4ly.alcantara@gmail.com>
2024-10-25 18:50:06 -0300

    Refactor Next.js configuration files and styles

--- next/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <section className="flex flex-col items-center justify-center pt-10 h-full">
-      <h1 className="bg-red-500 p-[200px] rounded-xl">oi</h1>
-    </section>
-  );
-}
