ebba3d849e7dcf04eca6b17924bf167054fe41ce
4lysson-a <4ly.alcantara@gmail.com>
2024-10-26 14:32:55 -0300

    Refactor layout and page components, add SingleCard component and update Next.js configuration

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,8 +1,13 @@
 import SingleCard from "@/components/layout/cards";
 
-export default function Home() {
+const user = "4lysson-a";
+const repo = "csgo-bind-view";
+const filePath = "binds.txt";
+const url = `https://api.github.com/repos/${user}/${repo}/contents/${filePath}?ref=base`;
+
+export default async function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-10 py-10 px-6">
+    <section className="flex flex-col items-center justify-center gap-10 py-10 px-6 w-full">
       <div>
         <h1 className="flex flex-col text-xl leading-4">
           <span className="text-5xl font-bold">Counter Strike</span>My binds
@@ -10,12 +15,18 @@ export default function Home() {
         </h1>
       </div>
 
-      <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-6">
-        <SingleCard
-          code="bind f1 say_team gg"
-          description="Say gg in team chat"
-          title="GG"
-        />
+      <div className="w-full h-full flex items-center justify-center">
+        <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-6 w-full sm:max-w-[70%] lg:max-w-[90%] xl:max-w-[80%] mx-auto">
+          {Array.from({ length: 6 }).map((_, index) => (
+            <SingleCard
+              key={index}
+              code="bind f1 say_team gg"
+              description="Say gg in team chat"
+              index={index}
+              title="GG"
+            />
+          ))}
+        </div>
       </div>
     </section>
   );
--- next/components/layout/cards/index.tsx
+++ next/components/layout/cards/index.tsx
@@ -1,27 +1,34 @@
 import { Card, CardBody, CardHeader } from "@nextui-org/card";
+import { Divider } from "@nextui-org/divider";
 import { Suspense } from "react";
 
 import CopyBtn from "./copy-btn";
 import CopyBtnShimmer from "./copy-btn/shimmer";
 
 interface Props {
-  description: string;
-  title: string;
   code: string;
+  index: number;
+  title: string;
+  description: string;
 }
 
-export default function SingleCard({ description, code, title }: Props) {
+export default function SingleCard({ description, code, title, index }: Props) {
   return (
-    <Card className="max-w-[400px]">
-      <CardHeader className="flex w-full justify-between items-center">
-        <p className="text-md">{title}</p>
+    <Card
+      className="w-full opacity-0 animate-fade-up animate-duration-300 animate-ease-in-out animate-fill-forwards"
+      style={{ animationDelay: `${index * 0.05}s` }}
+    >
+      <CardHeader className="flex w-full justify-between items-center px-6">
+        <p className="text-xl font-bold">{title}</p>
 
         <Suspense fallback={<CopyBtnShimmer />}>
           <CopyBtn text={code} />
         </Suspense>
       </CardHeader>
 
-      <CardBody className="flex flex-col gap-2">
+      <Divider />
+
+      <CardBody className="flex flex-col gap-2 px-6 pb-6">
         <p>{description}</p>
         <div className="bg-foreground-100 p-2 rounded-xl">{code}</div>
       </CardBody>
