4da25bc3bc18936197cf64e1e330fdd1212d7307
4lysson-a <4ly.alcantara@gmail.com>
2024-10-26 15:36:06 -0300

    Add .env.dist file and update .gitignore

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -1,10 +1,5 @@
 import SingleCard from "@/components/layout/cards";
 
-const user = "4lysson-a";
-const repo = "csgo-bind-view";
-const filePath = "binds.txt";
-const url = `https://api.github.com/repos/${user}/${repo}/contents/${filePath}?ref=base`;
-
 export default async function Home() {
   return (
     <section className="flex flex-col items-center justify-center gap-10 py-10 px-6 w-full">
@@ -16,7 +11,7 @@ export default async function Home() {
       </div>
 
       <div className="w-full h-full flex items-center justify-center">
-        <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-6 w-full sm:max-w-[70%] lg:max-w-[90%] xl:max-w-[80%] mx-auto">
+        <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-10 w-full sm:max-w-[70%] lg:max-w-[90%] xl:max-w-[80%] mx-auto">
           {Array.from({ length: 6 }).map((_, index) => (
             <SingleCard
               key={index}
