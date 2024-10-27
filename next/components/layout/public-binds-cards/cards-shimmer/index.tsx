2851e306359165109dc7fdb9f6b51565c937877a
4lysson-a <4ly.alcantara@gmail.com>
2024-10-27 14:15:07 -0300

    Refactor CardsShimmer component: update number of cards to render

--- next/components/layout/public-binds-cards/cards-shimmer/index.tsx
+++ next/components/layout/public-binds-cards/cards-shimmer/index.tsx
@@ -1,7 +1,7 @@
 import SingleCardShimmer from "@/components/shared/cards/single-card-shimmer";
 
 export default function CardsShimmer() {
-  return Array.from({ length: 6 }).map((_, index) => (
+  return Array.from({ length: 9 }).map((_, index) => (
     <SingleCardShimmer
       key={index}
       style={{
