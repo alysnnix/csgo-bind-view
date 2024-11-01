0c735072f2869b8fe4ffd0251ad4720bed0b34a9
4lysson-a <4ly.alcantara@gmail.com>
2024-11-01 13:49:11 -0300

    refact: first commit v1.0.0

---
+++ README.md
@@ -0,0 +1,16 @@
+# Visualizador de binds para o `CSGO`
+
+![image](https://user-images.githubusercontent.com/26152669/235336517-b01cd1b5-2e60-4670-84d4-42ecc1cf7d23.png)
+
+## Como funciona ?
+
+A ideia é ser uma página simples contendo várias binds para que seja facil a utilização.
+
+## [bind.alysson.dev](https://4lysson-a.github.io/csgo-bind-view/)
+
+Basta acessar a URL `bind.alysson.dev` no navegador dentro do próprio jogo e copiar a bind que desejar
+
+## [Base de dados](https://github.com/4lysson-a/csgo-bind-view/tree/base)
+
+Para contribuir com alguma bind acesse a base de dados
+
---
+++ src/index.js
@@ -0,0 +1,108 @@
+let splitedBind = [];
+let loading = true;
+
+function decodeContent(content) {
+	const decoder = new TextDecoder('utf-8');
+	const uint8Array = new Uint8Array([...window.atob(content)].map((char) => char.charCodeAt(0)));
+	return decoder.decode(uint8Array);
+}
+
+async function handleGetBinds() {
+	const user = '4lysson-a';
+	const repo = 'csgo-bind-view';
+	const filePath = 'binds.txt';
+	const url = `https://api.github.com/repos/${user}/${repo}/contents/${filePath}?ref=base`;
+
+	try {
+		const response = await fetch(url);
+
+		if (!response.ok) {
+			throw new Error(`Erro ao obter o arquivo: ${response.status} - ${response.statusText}`);
+		}
+
+		const file = await response.json();
+
+		const contentBase64 = file.content;
+		const content = decodeContent(contentBase64);
+		splitedBind = content.split('---');
+	} catch (error) {
+		console.error(error);
+	} finally {
+		loading = false;
+	}
+}
+
+const createDetails = () => {
+	const splitBindMap = splitedBind.map((item, index) => {
+		const [title, content] = item.split('||');
+		const validateBreakLine = content?.includes('<br />') ? '<br>' : '';
+		const validateHaveIqual = content?.includes('=') ? '=' : '';
+		
+		if (validateHaveIqual) {
+			const splitContent = content.split('<br />');
+			const structure = splitContent.map((item) => {
+				const [before, after] = item.split('=');
+				return `
+          <div class="bind-with-title-content">
+            <strong>${before}</strong>
+            <p>${after}</p>
+          </div>
+        `;
+			});
+			return template(structure.join(''));
+		}
+
+		if (validateBreakLine) {
+			const splitContent = content.split('<br />');
+			const structure = splitContent.map((item) => {
+				return `<p>${item}</p>`;
+			});
+
+			return template(structure.join(''));
+		}
+
+		let view = '';
+
+		function template(content) {
+			const [key, description] = title.split('-')
+			return `
+				<details>
+					<summary class="bind-title">
+						<strong class="key">${key}</strong>
+						${description || ''}
+						<button onclick="copyToClipboard('txt-${index}')">copiar</button>
+					</summary>
+					${content}
+				</details>
+			`;
+		}
+
+		// se o conteúdo tiver quebra de linha exiba em <p> diferentes
+		if (validateBreakLine) {
+			const splitContent = content.split('<br />');
+			const structure = splitContent.map((item, index) => {
+				return `<p>${item}</p>`;
+			});
+			return template(structure.join(''));
+		}
+
+		view = ` <p id="txt-${index}">${content} ${validateBreakLine}</p> `;
+
+		const structure = template(view);
+		return structure;
+	});
+
+	const removeLastElement = splitBindMap.splice(0, splitBindMap.length - 1);
+	return `${removeLastElement.join('')}`;
+};
+
+async function main() {
+	await handleGetBinds();
+	if (loading) {
+		document.body.insertAdjacentHTML('beforeend', '<h1>Carregando...</h1>');
+	} else {
+		document.body.insertAdjacentHTML('beforeend', createDetails());
+	}
+}
+
+main();
